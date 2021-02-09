88041663454cc53b0a435ef0257ace335a0f5cb3
egesha <egesha@danwin1210.me>
2021-02-09 19:06:19 +0300

    :hammer: Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import close as close_tortoise
 from .database import init as init_tortoise
@@ -6,6 +7,17 @@ from .routes import auth_router
 from .routes import weight_router
 
 app = FastAPI()
+
+origins = ["http://localhost:3000", "http://localhost"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router, prefix="/auth")
 app.include_router(weight_router, prefix="/weight")
 
